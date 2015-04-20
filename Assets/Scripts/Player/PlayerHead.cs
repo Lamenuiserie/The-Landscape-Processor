0629f59f2e0d07e694cb0363b1b87360c057172f
Simon Chauvin <contact@simonchauvin.com>
2015-04-20 05:01:24 +0200

    Problematic head movement script.

--- Assets/Scripts/Player/PlayerHead.cs
+++ Assets/Scripts/Player/PlayerHead.cs
@@ -14,57 +14,29 @@ public class PlayerHead : MonoBehaviour
     public float smoothTime = 5f;
 
 
-    private Quaternion m_CharacterTargetRot;
-    private Quaternion m_CameraTargetRot;
+    public Transform cursor;
 
 
-    public void Init(Transform character, Transform camera)
+    private Camera mainCamera;
+
+
+    void Start ()
     {
-        m_CharacterTargetRot = character.localRotation;
-        m_CameraTargetRot = camera.localRotation;
+        mainCamera = GetComponent<Camera>();
     }
 
-
-    public void LookRotation(Transform character, Transform camera)
+    void Update ()
     {
-        float yRot = CrossPlatformInputManager.GetAxis("Mouse X") * XSensitivity;
-        float xRot = CrossPlatformInputManager.GetAxis("Mouse Y") * YSensitivity;
+        //transform.LookAt(cursor);
+        Ray ray = mainCamera.ScreenPointToRay(CrossPlatformInputManager.mousePosition);
 
-        m_CharacterTargetRot *= Quaternion.Euler(0f, yRot, 0f);
-        m_CameraTargetRot *= Quaternion.Euler(-xRot, 0f, 0f);
+        Debug.DrawRay(ray.origin, ray.direction, Color.red);
 
-        if (clampVerticalRotation)
-            m_CameraTargetRot = ClampRotationAroundXAxis(m_CameraTargetRot);
-
-        if (smooth)
+        Quaternion rotationToMake = Quaternion.FromToRotation(transform.forward, ray.direction);
+        if (rotationToMake.eulerAngles.y >= 324 && rotationToMake.eulerAngles.y <= 335)
         {
-            character.localRotation = Quaternion.Slerp(character.localRotation, m_CharacterTargetRot,
-                smoothTime * Time.deltaTime);
-            camera.localRotation = Quaternion.Slerp(camera.localRotation, m_CameraTargetRot,
-                smoothTime * Time.deltaTime);
-        }
-        else
-        {
-            character.localRotation = m_CharacterTargetRot;
-            camera.localRotation = m_CameraTargetRot;
+            Debug.Log(transform.rotation.eulerAngles);
+            transform.rotation = rotationToMake;
         }
     }
-
-
-    Quaternion ClampRotationAroundXAxis(Quaternion q)
-    {
-        q.x /= q.w;
-        q.y /= q.w;
-        q.z /= q.w;
-        q.w = 1.0f;
-
-        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
-
-        angleX = Mathf.Clamp(angleX, MinimumX, MaximumX);
-
-        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
-
-        return q;
-    }
-
 }
