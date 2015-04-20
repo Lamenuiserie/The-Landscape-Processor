d8ca241c3e697457d92e82cb8430ce2b32ee9e14
Simon Chauvin <contact@simonchauvin.com>
2015-04-20 05:01:41 +0200

    No more warping.

--- Assets/Scripts/Selectables/EndArea.cs
+++ Assets/Scripts/Selectables/EndArea.cs
@@ -3,16 +3,13 @@ using System.Collections;
 
 public class EndArea : MonoBehaviour
 {
-    /// <summary>
-    /// The selectable manager;
-    /// </summary>
-    private SelectableManager selectableManager;
+
 
 
 	// Use this for initialization
 	void Start ()
     {
-        selectableManager = GetComponentInParent<SelectableManager>();
+        
 	}
 
     void OnTriggerEnter (Collider collider)
