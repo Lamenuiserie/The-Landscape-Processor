0f3d7758b43cb2130113627b8647017190e98eef
Simon Chauvin <contact@simonchauvin.com>
2015-05-04 15:28:08 +0200

    Window movement script now generic.

--- Assets/Scripts/World/Window.cs
+++ Assets/Scripts/World/Window.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class Window : MonoBehaviour
 {
-    
+    private float lowestYPosition;
+    private float highestYPosition;
+
     private float startYPosition;
     private float endYPosition;
     private float movementTimer;
@@ -13,8 +15,10 @@ public class Window : MonoBehaviour
 	void Start ()
     {
         movementTimer = 0f;
-        startYPosition = 0f;
-        endYPosition = 0.1f;
+        startYPosition = transform.position.y;
+        endYPosition = startYPosition + 0.1f;
+        lowestYPosition = startYPosition;
+        highestYPosition = endYPosition;
 	}
 	
 	// Update is called once per frame
@@ -23,16 +27,16 @@ public class Window : MonoBehaviour
         movementTimer += Time.deltaTime;
         transform.position = new Vector3(transform.position.x, Mathf.Lerp(startYPosition, endYPosition, movementTimer / 0.5f), transform.position.z);
 
-	    if (transform.position.y >= 0.1f)
+	    if (transform.position.y >= highestYPosition)
         {
-            startYPosition = 0.1f;
-            endYPosition = 0f;
+            startYPosition = highestYPosition;
+            endYPosition = lowestYPosition;
             movementTimer = 0f;
         }
-        else if (transform.position.y <= 0f)
+        else if (transform.position.y <= lowestYPosition)
         {
-            startYPosition = 0f;
-            endYPosition = 0.1f;
+            startYPosition = lowestYPosition;
+            endYPosition = highestYPosition;
             movementTimer = 0f;
         }
         
