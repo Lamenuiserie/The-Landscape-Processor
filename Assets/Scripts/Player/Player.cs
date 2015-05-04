9274379ef94cd9351eb5bf1e14f60e9dd5228f16
Simon Chauvin <contact@simonchauvin.com>
2015-05-04 15:32:29 +0200

    A Player script for handling a possible feature that slows the train down.

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    /// <summary>
+    /// The only instance of the player.
+    /// </summary>
+    private static Player _instance;
+
+    private SelectableManager selectableManager;
+    private GroundManager groundManager;
+
+    private int numberOfSelectablesSelected;
+
+
+    /// <summary>
+    /// Retrieve the instance of the audio manager.
+    /// </summary>
+    /// <value>The audio manager.</value>
+    public static Player instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.Find("Player").GetComponent<Player>();
+            }
+            return _instance;
+        }
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        selectableManager = GameObject.Find("SelectableManager").GetComponent<SelectableManager>();
+        groundManager = GameObject.Find("GroundManager").GetComponent<GroundManager>();
+
+        numberOfSelectablesSelected = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        /*if (32f - numberOfSelectablesSelected / 2f >= 0f)
+        {
+            selectableManager.groundSpeed = 32f - numberOfSelectablesSelected / 2f;
+            selectableManager.mountainSpeed = 32f - numberOfSelectablesSelected / 2f;
+            selectableManager.maxCloudSpeed = 10f - numberOfSelectablesSelected / 2f;
+            groundManager.speed = 32f - numberOfSelectablesSelected / 2f;
+        }*/
+	}
+
+    public void incSelectable ()
+    {
+        numberOfSelectablesSelected++;
+    }
+
+    public void decSelectable()
+    {
+        numberOfSelectablesSelected--;
+    }
+}
